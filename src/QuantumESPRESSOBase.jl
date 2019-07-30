30e6322f7759598012b44a04c64b48f29bb50d36
Qi Zhang <singularitti@outlook.com>
2019-07-30 02:18:40 -0400

    Fix inclusion

--- src/QuantumESPRESSOBase.jl
+++ src/QuantumESPRESSOBase.jl
@@ -4,6 +4,8 @@ export InputEntry
 
 abstract type InputEntry end
 
+include("Namelists/Namelists.jl")
 include("Cards/Cards.jl")
+include("QuantumESPRESSOInput/QuantumESPRESSOInput.jl")
 
 end # module
