6b0761225f1d503086fbd05ea3fd96b887e8d46b
Qi Zhang <singularitti@outlook.com>
2019-08-08 05:29:44 -0400

    Fix a typo in test

--- test/NamelistsTests.jl
+++ test/NamelistsTests.jl
@@ -15,7 +15,7 @@ using QuantumESPRESSOBase.Inputs.PW
 as = AtomicSpeciesCard([AtomicSpecies("Fe", 55.845, "Fe.pseudopotential")])
 ap = AtomicPositionsCard(data=[AtomicPosition(atom="Fe", pos=[0, 0, 0])])
 cell = CellParametersCard(data=ones(3, 3))
-k = KPointsCard(option="automatic", data=[GammaPoint()])
+k = KPointsCard(option="gamma", data=[GammaPoint()])
 
 pw = PWInput(system=SystemNamelist(celldm=[1]), atomic_species=as, atomic_positions=ap,
     k_points=k, cell_parameters=cell
