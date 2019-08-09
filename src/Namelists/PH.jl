4b0e551f5c004130f9cecc751c94bdacec22372c
Qi Zhang <singularitti@outlook.com>
2019-08-09 00:05:43 -0400

    Update default values in `INPUTPHNamelist ` according to QE

--- src/Namelists/PH.jl
+++ src/Namelists/PH.jl
@@ -18,19 +18,19 @@ using QuantumESPRESSOBase.Namelists: Namelist
 export INPUTPHNamelist
 
 @with_kw struct INPUTPHNamelist <: Namelist
-    amass::Vector{Union{Missing, Float64}} = zeros(5)
+    amass::Vector{Union{Missing, Float64}} = [0.0]
     outdir::String = "./"
     prefix::String = "pwscf"
     niter_ph::Int = 100
     tr2_ph::Float64 = 1e-12
-    alpha_mix::Float64 = 0.7
+    alpha_mix::Vector{Union{Missing, Float64}} = 0.7 * ones(niter_ph)
     nmix_ph::Int = 4
     verbosity::String = "default"
     reduce_io::Bool = false
     max_seconds::Float64 = 1e7
     fildyn::String = "matdyn"
-    fildrho::String = ""
-    fildvscf::String = ""
+    fildrho::String = " "
+    fildvscf::String = " "
     epsil::Bool = false
     lrpa::Bool = false
     lnoloc::Bool = false
@@ -45,9 +45,9 @@ export INPUTPHNamelist
     qplot::Bool = false
     q2d::Bool = false
     q_in_band_form::Bool = false
-    electron_phonon::String = ""
+    electron_phonon::String = " "
     lshift_q::Bool = false
-    zeu::Bool = epsil
+    zeu::Bool = epsil  # The default value in QE's source code is `true`
     zue::Bool = false
     elop::Bool = false
     fpol::Bool = false
@@ -66,12 +66,12 @@ export INPUTPHNamelist
     k1::Int = 0
     k2::Int = 0
     k3::Int = 0
-    start_irr::Int = 1
-    last_irr::Int = 3
+    start_irr::Int = 1  # The default value in QE's source code is 0
+    last_irr::Int = -1000
     nat_todo::Int = 0
     modenum::Int = 0
     start_q::Int = 1
-    last_q::Int = 10
+    last_q::Int = -1000
     # dvscf_star::String = 1
     # drho_star::String = 1
 end  # struct INPUTPHNamelist
