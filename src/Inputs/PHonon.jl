1ee774782a226d5b03898a03f952a3bbbe4fc25c
Qi Zhang <singularitti@outlook.com>
2019-08-19 08:36:00 -0400

    Implement `Q2RInput`

--- src/Inputs/PHonon.jl
+++ src/Inputs/PHonon.jl
@@ -18,12 +18,19 @@ using QuantumESPRESSOBase.Namelists.PHonon
 using QuantumESPRESSOBase.Cards.PHonon
 using QuantumESPRESSOBase.Inputs
 
-export PhononInput
+export PhononInput, Q2RInput
 
 @with_kw struct PhononInput <: AbstractInput
     phonon::PhononNamelist = PhononNamelist()
     q_points::QPointsSpecsCard
     @assert phonon.qplot == true
 end  # struct PhononInput
+
+@with_kw struct Q2RInput <: AbstractInput
+    q2r::Q2RNamelist = Q2RNamelist()
+    grid::AbstractVector
+    nqs::Int
+    @assert length(grid) == 3
+end
 
 end
