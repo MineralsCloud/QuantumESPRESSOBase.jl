64ccc7b1191fcf98cdb4dbf1c2a78b161a4fbf39
Qi Zhang <singularitti@outlook.com>
2019-08-19 08:30:00 -0400

    Add `PHonon` module in `Inputs` subpackage

---
+++ src/Inputs/PHonon.jl
@@ -0,0 +1,28 @@
+"""
+# module PHonon
+
+
+
+# Examples
+
+```jldoctest
+julia>
+```
+"""
+module PHonon
+
+using Parameters: @with_kw
+
+using QuantumESPRESSOBase.Namelists
+using QuantumESPRESSOBase.Namelists.PHonon
+using QuantumESPRESSOBase.Cards.PHonon
+using QuantumESPRESSOBase.Inputs
+
+export PhononInput
+
+@with_kw struct PhononInput <: AbstractInput
+    phonon::PhononNamelist = PhononNamelist()
+    q_points::QPointsSpecsCard
+end  # struct PhononInput
+
+end
