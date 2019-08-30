bb38cbbed7bcbc8bce22ff06150f624015cae602
Qi Zhang <singularitti@outlook.com>
2019-08-29 22:02:57 -0400

    docs: Add docs to `bravais_lattice`

--- src/bravais.jl
+++ src/bravais.jl
@@ -2,7 +2,17 @@ using QuantumESPRESSOBase.Namelists.PWscf
 
 export bravais_lattice
 
+"""
+    bravais_lattice(nml::SystemNamelist)
+
+Return a 3x3 matrix representing the Bravais lattice from `nml`.
+"""
 bravais_lattice(nml::SystemNamelist) = bravais_lattice(nml.ibrav, nml.celldm)
+"""
+    bravais_lattice(ibrav::Integer, celldm::AbstractVector{Union{Missing, Float64}})
+
+Return a 3x3 matrix representing the Bravais lattice from `ibrav` and `celldm`.
+"""
 bravais_lattice(ibrav::Integer, celldm::AbstractVector{Union{Missing, Float64}}) = bravais_lattice(Val(ibrav), celldm)
 bravais_lattice(::Val{1}, celldm::AbstractVector{Union{Missing, Float64}}) = celldm[1] * [
     1 0 0
