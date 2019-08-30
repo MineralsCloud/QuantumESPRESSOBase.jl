49f2e284e87a7e53c680d9491e266ab81758f3f6
Qi Zhang <singularitti@outlook.com>
2019-08-29 22:09:40 -0400

    docs: Add docs to `option`

--- src/Cards/option.jl
+++ src/Cards/option.jl
@@ -4,17 +4,12 @@ using QuantumESPRESSOBase.Cards.PWscf
 export option
 
 """
-    option()
+    option(x::Card)
 
+Return the option for `Card` `x`.
 
-
-# Arguments
-
-# Examples
-
-```jldoctest
-julia>
-```
+A user should not use `x.option` to access a `Card`'s `option`. Because some `Card`s do not have an option.
+Using `option(x)` is suggested.
 """
 option(card::Card) = getfield(card, :option)
 option(card::AtomicSpeciesCard) = nothing
