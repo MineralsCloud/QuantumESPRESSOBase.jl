ceea1197f3cbec06239980318e2ea6112e7000cf
Qi Zhang <singularitti@outlook.com>
2019-09-09 23:23:39 -0400

    Rename bravais.jl to bravais_lattice.jl
    the same name as the function

--- src/QuantumESPRESSOBase.jl
+++ src/QuantumESPRESSOBase.jl
@@ -6,7 +6,7 @@ abstract type InputEntry end
 
 include("Namelists/Namelists.jl")
 include("Cards/Cards.jl")
-include("bravais.jl")
+include("bravais_lattice.jl")
 include("Inputs/Inputs.jl")
 include("name.jl")
 include("to_qe.jl")
