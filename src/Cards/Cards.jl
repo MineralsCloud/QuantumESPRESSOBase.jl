e7d974bf839c3faf00cdf9117716e60385a25d99
Qi Zhang <singularitti@outlook.com>
2019-09-16 02:25:59 -0400

    Move type `Card` to src/Cards/prelude.jl

--- src/Cards/Cards.jl
+++ src/Cards/Cards.jl
@@ -17,13 +17,7 @@ using QuantumESPRESSOBase: InputEntry
 
 export Card, option, allowed_options
 
-abstract type Card <: InputEntry end
-
-function Parameters.reconstruct(card::Card, newdict::AbstractDict)
-    :option in keys && error("If you want to change the option of a card, reconstruct a new one!")
-    return reconstruct(card, newdict)
-end # function reconstruct
-
+include("prelude.jl")
 include("PWscf.jl")
 include("option.jl")
 include("allowed_options.jl")
