b3246d2397ca6365d30771038fb6e1f00166571e
Qi Zhang <singularitti@outlook.com>
2019-10-19 18:50:08 -0400

    Move commond `Card`s of `PWscf` & `CP` to src/Cards/common.jl

---
+++ src/Cards/CP.jl
@@ -0,0 +1,17 @@
+module CP
+
+using QuantumESPRESSOBase.Cards: AtomicSpecies,
+    AtomicSpeciesCard,
+    AtomicPosition,
+    AtomicPositionsCard,
+    CellParametersCard,
+    pseudopotential_format
+
+export AtomicSpecies,
+    AtomicSpeciesCard,
+    AtomicPosition,
+    AtomicPositionsCard,
+    CellParametersCard,
+    pseudopotential_format
+
+end
--- src/Cards/Cards.jl
+++ src/Cards/Cards.jl
@@ -11,14 +11,18 @@ julia>
 """
 module Cards
 
-using Parameters
+using MLStyle: @match
+using Parameters: @with_kw
+using Setfield: @lens, get
 
 using QuantumESPRESSOBase: InputEntry
 
 export Card, option, allowed_options
 
 include("prelude.jl")
+include("common.jl")
 include("PWscf.jl")
+include("CP.jl")
 include("PHonon.jl")
 include("option.jl")
 include("allowed_options.jl")
