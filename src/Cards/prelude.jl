9a523adf7c572a81c6ad33a857602a9fc9edb3c4
Qi Zhang <singularitti@outlook.com>
2019-10-19 05:00:45 -0400

    Deprecate `Parameters.reconstruct` in src/Cards/prelude.jl
    use `Setfield` always

--- src/Cards/prelude.jl
+++ src/Cards/prelude.jl
@@ -1,6 +1,1 @@
 abstract type Card <: InputEntry end
-
-function Parameters.reconstruct(card::Card, newdict::AbstractDict)
-    :option in keys && error("If you want to change the option of a card, reconstruct a new one!")
-    return reconstruct(card, newdict)
-end # function reconstruct
