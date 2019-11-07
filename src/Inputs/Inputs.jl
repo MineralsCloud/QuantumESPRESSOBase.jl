4497aa0e84b938968feb8481eb9a8703bb9898ee
Qi Zhang <singularitti@outlook.com>
2019-11-06 23:07:29 -0500

    :fire: Move src/Inputs/prelude.jl to Inputs.jl

--- src/Inputs/Inputs.jl
+++ src/Inputs/Inputs.jl
@@ -14,7 +14,28 @@ module Inputs
 using QuantumESPRESSOBase.Namelists
 using QuantumESPRESSOBase.Cards
 
-include("prelude.jl")
+export AbstractInput, namelists, cards
+
+abstract type AbstractInput end
+
+# A helper function to implement `namelists` and `cards`. It should not be exported.
+_filterfields(obj, ::Type{T}) where {T} =
+    filter(x -> isa(x, T), map(x -> getfield(obj, x), fieldnames(typeof(obj))) |> collect)
+
+"""
+    namelists(input::AbstractInput)
+
+Return a vector of `Namelist`s of a `AbstractInput`'s subtypes.
+"""
+namelists(input::AbstractInput) = _filterfields(input, Namelist)
+
+"""
+    cards(input::AbstractInput)
+
+Return a vector of `Card`s of a `AbstractInput`'s subtypes.
+"""
+cards(input::AbstractInput) = _filterfields(input, Card)
+
 include("PWscf.jl")
 include("PHonon.jl")
 
