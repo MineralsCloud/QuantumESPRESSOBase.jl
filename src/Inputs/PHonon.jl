e9868fc64160c7874437a08ffcc48b6fd6df6c49
Qi Zhang <singularitti@outlook.com>
2019-11-15 04:28:33 -0500

    Rename `Q2RInput` to `Q2rInput`

--- src/Inputs/PHonon.jl
+++ src/Inputs/PHonon.jl
@@ -18,14 +18,14 @@ using QuantumESPRESSOBase.Namelists.PHonon
 using QuantumESPRESSOBase.Cards.PHonon
 using QuantumESPRESSOBase.Inputs: QuantumESPRESSOInput
 
-export PhInput, Q2RInput, MatdynInput, DynmatInput
+export PhInput, Q2rInput, MatdynInput, DynmatInput
 
 @with_kw struct PhInput <: QuantumESPRESSOInput
     inputph::PhNamelist = PhNamelist()
     q_points::Union{Nothing,QPointsSpecsCard} = nothing
 end # struct PhInput
 
-@with_kw struct Q2RInput <: QuantumESPRESSOInput
+@with_kw struct Q2rInput <: QuantumESPRESSOInput
     input::Q2rNamelist = Q2rNamelist()
 end
 
