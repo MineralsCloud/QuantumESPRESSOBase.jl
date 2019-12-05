2f467025d7b5058266d7b8d2b60c90b883e0306c
Qi Zhang <singularitti@outlook.com>
2019-12-05 00:34:11 -0500

    Separate API `bravais_lattice` & `_bravais_lattice` helpers

--- src/bravais_lattice.jl
+++ src/bravais_lattice.jl
@@ -14,38 +14,39 @@ bravais_lattice(nml::SystemNamelist) = bravais_lattice(nml.ibrav, nml.celldm)
 Return a 3x3 matrix representing the Bravais lattice from `ibrav` and `celldm`.
 """
 bravais_lattice(ibrav::Integer, celldm::AbstractVector{Union{Nothing,Float64}}) =
-    bravais_lattice(Val(ibrav), celldm)
-bravais_lattice(::Val{1}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+    _bravais_lattice(Val(ibrav), celldm)
+# These are helper methods and should not be exported.
+_bravais_lattice(::Val{1}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] * [
         1 0 0
         0 1 0
         0 0 1
     ]
-bravais_lattice(::Val{2}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+_bravais_lattice(::Val{2}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] / 2 * [
         -1 0 1
         0 1 1
         -1 1 0
     ]
-bravais_lattice(::Val{3}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+_bravais_lattice(::Val{3}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] / 2 * [
         1 1 1
         -1 1 1
         -1 -1 1
     ]
-bravais_lattice(::Val{-3}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+_bravais_lattice(::Val{-3}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] / 2 * [
         -1 1 1
         1 -1 1
         1 1 -1
     ]
-bravais_lattice(::Val{4}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+_bravais_lattice(::Val{4}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] * [
         1 0 0
         -1 / 2 sqrt(3) / 2 0
         0 0 celldm[3]
     ]
-function bravais_lattice(::Val{5}, celldm::AbstractVector{Union{Nothing,Float64}})
+function _bravais_lattice(::Val{5}, celldm::AbstractVector{Union{Nothing,Float64}})
     c = celldm[3]
     tx = sqrt((1 - c) / 2)
     ty = sqrt((1 - c) / 6)
@@ -56,7 +57,7 @@ function bravais_lattice(::Val{5}, celldm::AbstractVector{Union{Nothing,Float64}
         -tx -ty tz
     ]
 end
-function bravais_lattice(::Val{-5}, celldm::AbstractVector{Union{Nothing,Float64}})
+function _bravais_lattice(::Val{-5}, celldm::AbstractVector{Union{Nothing,Float64}})
     ap = celldm[1] / sqrt(3)
     c = celldm[3]
     ty = sqrt((1 - c) / 6)
@@ -69,79 +70,79 @@ function bravais_lattice(::Val{-5}, celldm::AbstractVector{Union{Nothing,Float64
         v v u
     ]
 end
-bravais_lattice(::Val{6}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+_bravais_lattice(::Val{6}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] * [
         1 0 0
         0 1 0
         0 0 celldm[3]
     ]
-bravais_lattice(::Val{7}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+_bravais_lattice(::Val{7}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] / 2 * [
         1 -1 celldm[3] / celldm[1]
         1 1 celldm[3] / celldm[1]
         -1 -1 celldm[3] / celldm[1]
     ]
-bravais_lattice(::Val{8}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+_bravais_lattice(::Val{8}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] * [
         1 0 0
         0 celldm[2] 0
         0 0 celldm[3]
     ]
-bravais_lattice(::Val{9}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+_bravais_lattice(::Val{9}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] * [
         1 / 2 celldm[2] / 2 0
         -1 / 2 celldm[2] / 2 0
         0 0 celldm[3]
     ]
-bravais_lattice(::Val{-9}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+_bravais_lattice(::Val{-9}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] * [
         1 / 2 -celldm[2] / 2 0
         1 / 2 celldm[2] / 2 0
         0 0 celldm[3]
     ]
-bravais_lattice(::Val{91}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+_bravais_lattice(::Val{91}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] * [
         1 0 0
         0 celldm[2] / 2 -celldm[3] / 2
         0 celldm[2] / 2 celldm[3] / 2
     ]
-bravais_lattice(::Val{10}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+_bravais_lattice(::Val{10}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] * [
         1 / 2 0 celldm[3] / 2
         1 / 2 celldm[2] / 2 0
         0 celldm[2] / 2 celldm[3] / 2
     ]
-bravais_lattice(::Val{11}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+_bravais_lattice(::Val{11}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] * [
         1 / 2 0 celldm[3] / 2
         1 / 2 celldm[2] / 2 0
         0 celldm[2] / 2 celldm[3] / 2
     ]
-bravais_lattice(::Val{12}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+_bravais_lattice(::Val{12}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] * [
         1 0 0
         celldm[2] * celldm[4] celldm[2] * sqrt(1 - celldm[4]^2) 0
         0 0 celldm[3]
     ]
-bravais_lattice(::Val{-12}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+_bravais_lattice(::Val{-12}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] * [
         1 0 0
         0 celldm[2] 0
         celldm[3] * celldm[5] 0 celldm[3] * sqrt(1 - celldm[5]^2)
     ]
-bravais_lattice(::Val{13}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+_bravais_lattice(::Val{13}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] * [
         1 / 2 0 -celldm[3] / 2
         celldm[2] * celldm[4] celldm[2] * sqrt(1 - celldm[4]^2) 0
         1 / 2 0 celldm[3] / 2
     ]
-bravais_lattice(::Val{-13}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+_bravais_lattice(::Val{-13}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] * [
         1 / 2 -celldm[2] / 2 0
         1 / 2 celldm[2] / 2 0
         celldm[3] * celldm[5] 0 celldm[3] * sqrt(1 - celldm[5]^2)
     ]
-bravais_lattice(::Val{14}, celldm::AbstractVector{Union{Nothing,Float64}}) =
+_bravais_lattice(::Val{14}, celldm::AbstractVector{Union{Nothing,Float64}}) =
     celldm[1] * [
         1 0 0
         celldm[2] * celldm[6] celldm[2] * sqrt(1 - celldm[6]^2) 0
