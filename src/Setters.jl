ecf7615465a49025c272fbd2e30b0bffd1ffb13a
Qi Zhang <singularitti@outlook.com>
2019-12-15 11:31:22 -0500

    :alembic: :sparkles: Add `VerbositySetter`

--- src/Setters.jl
+++ src/Setters.jl
@@ -1,8 +1,18 @@
 module Setters
 
+export VerbositySetter
+
 export batchset
 
 abstract type BatchSetter end
+struct VerbositySetter{T} <: BatchSetter
+    function VerbositySetter{T}() where {T}
+        T ∈ (:high, :low) ||
+        throw(ArgumentError("the type parameter must be either `:high` or `:low`!"))
+        return new()
+    end # function VerbositySetter{T}
+end
+VerbositySetter(x) = VerbositySetter{x}()
 
 function batchset end
 
