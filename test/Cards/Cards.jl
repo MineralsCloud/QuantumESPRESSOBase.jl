01c5a2265f41fc339117ff0d051ad4481d5a4519
Qi Zhang <singularitti@outlook.com>
2020-01-06 02:06:54 -0500

    :white_check_mark: Add more tests to "Test constructing `AtomicSpeciesCard` from `StructArray`s"

--- test/Cards/Cards.jl
+++ test/Cards/Cards.jl
@@ -8,12 +8,15 @@ using QuantumESPRESSOBase.Cards.PWscf
     atoms = ["Al", "As"]
     masses = [24590.7655930491, 68285.4024548272]
     pseudopotentials = ["Al.pbe-n-kjpaw_psl.1.0.0.UPF", "As.pbe-n-kjpaw_psl.1.0.0.UPF"]
-    @teest AtomicSpeciesCard(StructArray{AtomicSpecies}((
-        atoms,
-        masses,
-        pseudopotentials,
-    ))).data == AtomicSpecies[
+    init = AtomicSpeciesCard(StructArray{AtomicSpecies}((atoms, masses, pseudopotentials)))
+    @test init.data == [
         AtomicSpecies("Al", 24590.7655930491, "Al.pbe-n-kjpaw_psl.1.0.0.UPF"),
         AtomicSpecies("As", 68285.4024548272, "As.pbe-n-kjpaw_psl.1.0.0.UPF"),
+    ]
+    push!(init.data, AtomicSpecies("Si", 25591.1924913552, "Si.pbe-n-kjpaw_psl.1.0.0.UPF"))
+    @test init.data == [
+        AtomicSpecies("Al", 24590.7655930491, "Al.pbe-n-kjpaw_psl.1.0.0.UPF"),
+        AtomicSpecies("As", 68285.4024548272, "As.pbe-n-kjpaw_psl.1.0.0.UPF"),
+        AtomicSpecies("Si", 25591.1924913552, "Si.pbe-n-kjpaw_psl.1.0.0.UPF"),
     ]
 end # testset
