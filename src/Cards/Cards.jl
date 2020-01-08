7e2f288dee4641da98742aae242e3438d23ecb7e
Qi Zhang <singularitti@outlook.com>
2020-01-08 18:30:59 -0500

    Deprecate :== for `AtomicSpecies` & `AtomicPosition`
    Use `@auto_hash_equals` now

--- src/Cards/Cards.jl
+++ src/Cards/Cards.jl
@@ -418,8 +418,4 @@ function QuantumESPRESSOBase.to_qe(
     return content
 end
 
-function Base.:(==)(x::T, y::T) where {T<:Union{AtomicSpecies,AtomicPosition}}
-    return all(getfield(x, i) == getfield(y, i) for i in 1:fieldcount(T))
-end
-
 end
