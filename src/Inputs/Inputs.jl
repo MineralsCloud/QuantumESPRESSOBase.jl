1054257cea3fb6f95fd3e6916068f350b79a60a3
Qi Zhang <singularitti@outlook.com>
2020-01-08 01:53:19 -0500

    Rename arg `sep` to `delim` in src/Inputs/Inputs.jl

--- src/Inputs/Inputs.jl
+++ src/Inputs/Inputs.jl
@@ -121,15 +121,15 @@ end # function QuantumESPRESSOBase.cell_volume
 function QuantumESPRESSOBase.to_qe(
     input::QuantumESPRESSOInput;
     indent = ' '^4,
-    sep = ' ',
+    delim = ' ',
     verbose::Bool = false,
 )::String
     content = ""
     for namelist in namelists(input)
-        content *= to_qe(namelist, indent = indent, sep = sep, verbose = verbose)
+        content *= to_qe(namelist, indent = indent, delim = delim)
     end
     for card in cards(input)
-        content *= to_qe(card, indent = indent, sep = sep)
+        content *= to_qe(card, indent = indent, delim = delim)
     end
     return content
 end
