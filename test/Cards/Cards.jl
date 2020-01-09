214f9980502842d3c1a2ee3dfb72b23ccc1e7470
Qi Zhang <singularitti@outlook.com>
2020-01-09 17:08:52 -0500

    :white_check_mark: Add "Constructing `AtomicPosition`"

--- test/Cards/Cards.jl
+++ test/Cards/Cards.jl
@@ -46,6 +46,15 @@ end # testset
     end # testset
 end # testset
 
+@testset "Constructing `AtomicPosition`" begin
+    # Data from https://github.com/QEF/q-e/blob/7be27df/PW/examples/gatefield/run_example#L129-L132.
+    x = AtomicPosition("S", [0.500000000, 0.288675130, 1.974192764])
+    @test_throws AssertionError @set x.atom = "sulfur"
+    @test_throws TypeError @set x.pos = [1im, 2im, 3im]
+    @test x.if_pos == [1, 1, 1]
+    @test x == AtomicPosition('S', [0.500000000, 0.288675130, 1.974192764])
+end # testset
+
 @testset "Test constructing `AtomicPositionsCard` from `StructArray`s" begin
     # Data from https://github.com/QEF/q-e/blob/7be27df/PW/examples/gatefield/run_example#L129-L132.
     atoms = ["S", "Mo", "S"]
