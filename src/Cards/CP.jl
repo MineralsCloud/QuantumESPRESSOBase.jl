043c35fb204cdc9e0dccd99f01576fed2a816339
Qi Zhang <singularitti@outlook.com>
2020-01-10 17:38:27 -0500

    :sparkles: Make `AtomicVelocity` also mutable

--- src/Cards/CP.jl
+++ src/Cards/CP.jl
@@ -18,15 +18,28 @@ export pseudopot_format, option_convert
 
 include("shared.jl")
 # ============================== AtomicVelocity ============================== #
-@auto_hash_equals struct AtomicVelocity{A<:AbstractVector{<:Real}}
+@auto_hash_equals mutable struct AtomicVelocity
     atom::String
-    velocity::A
-    function AtomicVelocity{A}(atom, velocity) where {A<:AbstractVector{<:Real}}
+    velocity::Vector{Float64}
+    function AtomicVelocity(atom, velocity)
+        @assert(length(atom) <= 3, "Max total length of `atom` cannot exceed 3 characters!")
         @assert length(velocity) == 3
         return new(atom, velocity)
     end
+    function AtomicVelocity(atom::Union{AbstractChar,AbstractString})
+        @assert(length(atom) <= 3, "Max total length of `atom` cannot exceed 3 characters!")
+        return new(string(atom))
+    end
 end
-AtomicVelocity(atom, velocity::A) where {A} = AtomicVelocity{A}(atom, velocity)
+AtomicVelocity(x::AtomicSpecies, velocity) = AtomicVelocity(x.atom, velocity)
+AtomicVelocity(x::AtomicPosition, velocity) = AtomicVelocity(x.atom, velocity)
+AtomicVelocity(x::AtomicSpecies) = AtomicVelocity(x.atom)
+AtomicVelocity(x::AtomicPosition) = AtomicVelocity(x.atom)
+# Introudce mutual constructors since they share the same atoms.
+AtomicSpecies(x::AtomicVelocity, mass, pseudopot) = AtomicSpecies(x.atom, mass, pseudopot)
+AtomicSpecies(x::AtomicVelocity) = AtomicSpecies(x.atom)
+AtomicPosition(x::AtomicVelocity, pos, if_pos) = AtomicPosition(x.atom, pos, if_pos)
+AtomicPosition(x::AtomicVelocity) = AtomicPosition(x.atom)
 
 @auto_hash_equals struct AtomicVelocitiesCard{A<:AbstractVector{<:AtomicVelocity}} <: Card
     data::A
@@ -53,5 +66,17 @@ Cards.optionof(::AtomicForcesCard) = nothing
 
 Cards.allowed_options(::Type{<:AtomicVelocity}) = ("a.u",)
 Cards.allowed_options(::Type{<:RefCellParametersCard}) = ("bohr", "angstrom")
+
+function Base.setproperty!(value::AtomicVelocity, name::Symbol, x)
+    if name == :atom
+        @assert(length(x) <= 3, "Max total length of `atom` cannot exceed 3 characters!")
+        if x isa AbstractChar
+            x = string(x)
+        end
+    elseif name == :velocity && x isa AbstractVector  # It cannot be an `else` here, since it will capture invalid `name`s.
+        @assert length(x) == 3
+    end
+    setfield!(value, name, x)
+end # function Base.setproperty!
 
 end
