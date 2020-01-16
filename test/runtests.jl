a27f1279b6f332eaef670a24421cd770a769de2c
Qi Zhang <singularitti@outlook.com>
2020-01-16 01:29:59 -0500

    Add tests by jw3699@columbia.edu
    part of PR #69

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using QuantumESPRESSOBase
 using Test
 
 @testset "QuantumESPRESSOBase.jl" begin
+    include("prelude.jl")
     include("Inputs.jl")
     include("Cards.jl")
     include("Setters.jl")
