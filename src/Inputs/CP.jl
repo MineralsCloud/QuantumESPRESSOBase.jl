MineralsCloud/QuantumESPRESSOBase.jl

commit d4fc3f223d33d1faadcf0fd8d2b760869746dda7

Move `Maybe` to src/Inputs/shared.jl

src/Inputs/CP.jl
@@ -35,6 +35,8 @@ export AtomicSpecies,
     AtomicForcesCard
 export optconvert
 
+include("shared.jl")
+
 # The following default values are picked from `<QE source>/Modules/read_namelists.f90`
 """
     ControlNamelist <: Namelist
@@ -92,7 +94,7 @@ Represent the `SYSTEM` namelist of `cp.x`.
 """
 @with_kw struct SystemNamelist <: Namelist
     ibrav::Int = -1
-    celldm::Vector{Float64} = zeros(6)  # Must specify
+    celldm::Vector{Maybe{Float64}} = zeros(6)  # Must specify
     A::Float64 = 0.0
     B::Float64 = 0.0
     C::Float64 = 0.0
@@ -380,8 +382,6 @@ Setters.preset_values(::VerbositySetter{:low}, ::ControlNamelist) =
     ("low", false, false, false, false, "default")
 Setters.preset_values(::CalculationSetter{T}, ::ControlNamelist) where {T} =
     replace(string(T), "_" => "-")
-
-include("shared.jl")
 
 """
     AtomicVelocity(atom::Union{AbstractChar,String}, velocity::Vector{Float64})

src/Inputs/PWscf.jl
@@ -55,7 +55,7 @@ export AtomicSpecies,
     KPointsCard
 export optconvert
 
-const Maybe{T} = Union{T,Nothing}
+include("shared.jl")
 
 # The default values are from https://github.com/QEF/q-e/blob/4132a64/Modules/read_namelists.f90.
 """
@@ -121,7 +121,7 @@ Represent the `SYSTEM` namelist of `pw.x`.
 """
 @with_kw struct SystemNamelist <: Namelist
     ibrav::Int = 0  # The default value in QE's source code is -1
-    celldm::Vector{Float64} = zeros(6)  # Must specify
+    celldm::Vector{Maybe{Float64}} = zeros(6)  # Must specify
     A::Float64 = 0.0
     B::Float64 = 0.0
     C::Float64 = 0.0
@@ -506,8 +506,6 @@ function Setters.preset_values(::FiniteTemperatureSetter{N}, ::SystemNamelist) w
 end # function Setters.preset_values
 Setters.preset_values(::CalculationSetter{T}, ::ControlNamelist) where {T} =
     replace(string(T), "_" => "-")
-
-include("shared.jl")
 
 """
     PWInput <: QuantumESPRESSOInput

src/Inputs/shared.jl
@@ -11,6 +11,8 @@ import Crystallography
 import Pseudopotentials
 import QuantumESPRESSOBase.Inputs
 
+const Maybe{T} = Union{T,Nothing}
+
 # =============================== AtomicSpecies ============================== #
 """
     AtomicSpecies(atom::Union{AbstractChar,String}, mass::Float64, pseudopot::String)

commit e258f9c18bc720e555c3f04e341f08c9ba3c6d66

Fix the positions of `Maybe`
A bug introduced in d4fc3f223d33d1faadcf0fd8d2b760869746dda7

src/Inputs/CP.jl
@@ -34,7 +34,8 @@ export AtomicSpecies,
     AtomicForcesCard
 export optconvert
 
-include("shared.jl")
+# From https://discourse.julialang.org/t/aliases-for-union-t-nothing-and-union-t-missing/15402/4
+const Maybe{T} = Union{T,Nothing}
 
 # The following default values are picked from `<QE source>/Modules/read_namelists.f90`
 """
@@ -360,6 +361,8 @@ Input this namelist only if `calculation` is `"cp-wf"` or `"vc-cp-wf"`.
     @assert 1 <= calwf <= 5
     @assert 1 <= wfsd <= 3
 end # struct WannierNamelist
+
+include("shared.jl")
 
 function Setters.make(::LensMaker{VerbositySetter,ControlNamelist})
     return @batchlens begin

src/Inputs/PWscf.jl
@@ -55,7 +55,8 @@ export AtomicSpecies,
     KPointsCard
 export optconvert
 
-include("shared.jl")
+# From https://discourse.julialang.org/t/aliases-for-union-t-nothing-and-union-t-missing/15402/4
+const Maybe{T} = Union{T,Nothing}
 
 # The default values are from https://github.com/QEF/q-e/blob/4132a64/Modules/read_namelists.f90.
 """
@@ -452,7 +453,9 @@ Represent the `BANDS` namelist of `bands.x`.
     firstk::UInt = 0
     lastk::UInt = 10000000
     @assert spin_component ∈ 1:2
-end # struct BandsNamelist
+end # struct
+
+include("shared.jl")
 
 function Setters.make(::LensMaker{<:VerbositySetter,ControlNamelist})
     return @batchlens begin

src/Inputs/shared.jl
@@ -11,8 +11,6 @@ import Crystallography
 import Pseudopotentials
 import ..Inputs
 
-const Maybe{T} = Union{T,Nothing}
-
 # =============================== AtomicSpecies ============================== #
 """
     AtomicSpecies(atom::Union{AbstractChar,String}, mass::Float64, pseudopot::String)