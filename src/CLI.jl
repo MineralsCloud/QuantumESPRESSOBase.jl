be2de101cece209cc9e66bae9c1edb10b5461610
singularitti <singularitti@outlook.com>
2021-01-26 20:43:03 -0500

    Deprecate `bin` in `QuantumESPRESSOExec`, use preferences

--- src/CLI.jl
+++ src/CLI.jl
@@ -12,7 +12,6 @@ export PWExec, PhExec, Q2rExec, MatdynExec
 abstract type QuantumESPRESSOExec <: Executable end
 # See https://github.com/QEF/q-e/blob/884a6f8/Modules/command_line_options.f90
 struct PWExec <: QuantumESPRESSOExec
-    bin
     nimage::UInt
     npool::UInt
     ntg::UInt
@@ -20,25 +19,18 @@ struct PWExec <: QuantumESPRESSOExec
     nband::UInt
     ndiag::UInt
 end
-PWExec(; bin = "pw.x", nimage = 0, npool = 0, ntg = 0, nyfft = 0, nband = 0, ndiag = 0) =
-    PWExec(bin, nimage, npool, ntg, nyfft, nband, ndiag)
+PWExec(; nimage = 0, npool = 0, ntg = 0, nyfft = 0, nband = 0, ndiag = 0) =
+    PWExec(nimage, npool, ntg, nyfft, nband, ndiag)
 # See https://www.quantum-espresso.org/Doc/ph_user_guide/node14.html
 struct PhExec <: QuantumESPRESSOExec
-    bin
     nimage::UInt
     npool::UInt
 end
-PhExec(; bin = "ph.x", nimage = 0, npool = 0) = PhExec(bin, nimage, npool)
+PhExec(; nimage = 0, npool = 0) = PhExec(nimage, npool)
 
-struct Q2rExec <: QuantumESPRESSOExec
-    bin
-end
-Q2rExec(; bin = "q2r.x") = Q2rExec(bin)
+struct Q2rExec <: QuantumESPRESSOExec end
 
-struct MatdynExec <: QuantumESPRESSOExec
-    bin
-end
-MatdynExec(; bin = "q2r.x") = MatdynExec(bin)
+struct MatdynExec <: QuantumESPRESSOExec end
 
 function _prescriptify(  # Never export!
     x,
