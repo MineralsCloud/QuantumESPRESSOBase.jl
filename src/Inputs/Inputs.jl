9ea7a86bba388576f93d92b3bf02a91738d43b88
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-31 01:46:21 -0400

    Move things related to `asstring` to src/Inputs/asstring.jl

--- src/Inputs/Inputs.jl
+++ src/Inputs/Inputs.jl
@@ -11,17 +11,14 @@ julia>
 """
 module Inputs
 
-using AbInitioSoftwareBase.Inputs: Input, InputEntry, Namelist, Card, Setter
+using AbInitioSoftwareBase.Inputs: Input, InputEntry, Namelist, Card, Setter, groupname
 using Compat: only, isnothing
 using OptionalArgChecks: @argcheck
 using PyFortran90Namelists: fstring
 
-import AbInitioSoftwareBase.Inputs: asstring, groupname
-
 export optionof,
     optionpool,
     groupname,
-    asstring,
     required_namelists,
     optional_namelists,
     required_cards,
@@ -105,95 +102,10 @@ struct VerbositySetter <: Setter
     end
 end
 
+include("asstring.jl")
 include("PWscf/PWscf.jl")
 # include("CP/CP.jl")
 include("PHonon/PHonon.jl")
-
-"""
-    asstring(input::QuantumESPRESSOInput)
-
-Return a `String` representing a `QuantumESPRESSOInput`, valid for Quantum ESPRESSO's input.
-"""
-function asstring(input::QuantumESPRESSOInput)
-    return join(
-        map(
-            asstring,
-            Iterators.filter(!isnothing, getfield(input, i) for i in 1:nfields(input)),
-        ),
-        newline(input),
-    ) * newline(input)  # Add a new line at the end of line to prevent errors
-end
-"""
-    asstring(nml::Namelist)
-
-Return a `String` representing a `Namelist`, valid for Quantum ESPRESSO's input.
-"""
-function asstring(nml::Namelist)
-    content = _nmlasstring(
-        dropdefault(nml);
-        indent = indent(nml),
-        delimiter = delimiter(nml),
-        newline = newline(nml),
-    )
-    return join(filter(!isempty, ("&" * groupname(nml), content, '/')), newline(nml))
-end
-asstring(x::AbstractString) = string(x)
-function _nmlasstring(
-    dict::AbstractDict;
-    indent = ' '^4,
-    delimiter = ' ',
-    newline = '\n',
-)
-    return join(
-        map(keys(dict), values(dict)) do key, value
-            _nmlasstring(
-                key,
-                value;
-                indent = indent,
-                delimiter = delimiter,
-                newline = newline,
-            )
-        end,
-        newline,
-    )
-end
-function _nmlasstring(
-    key,
-    value::AbstractVector;
-    indent = ' '^4,
-    delimiter = ' ',
-    newline = '\n',
-)
-    return join(
-        (
-            indent * join((string(key, '(', i, ')'), "=", fstring(x)), delimiter) for
-            (i, x) in enumerate(value) if !isnothing(x)
-        ),
-        newline,
-    )
-end
-function _nmlasstring(
-    key,
-    value::NamedTuple;
-    indent = ' '^4,
-    delimiter = ' ',
-    newline = '\n',
-)
-    return join(
-        map(keys(value), values(value)) do x, y
-            indent * join((string(key, '%', x), "=", fstring(y)), delimiter)
-        end,
-        newline,
-    )
-end
-_nmlasstring(key, value; indent = ' '^4, delimiter = ' ', newline = '\n') =
-    indent * join((string(key), "=", fstring(value)), delimiter)
-
-newline(::Union{QuantumESPRESSOInput,Namelist,Card}) = '\n'
-
-indent(::Namelist) = ' '^4
-
-delimiter(::Namelist) = ' '
 
 include("crystallography.jl")
 
