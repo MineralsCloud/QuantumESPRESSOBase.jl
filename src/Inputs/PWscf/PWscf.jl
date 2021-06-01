478e3aa1efcb2ce71c5350c5225b6d8b1f5cd71f
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-31 21:22:36 -0400

    Move `iscompatible` to src/Inputs/PWscf/validation.jl

--- src/Inputs/PWscf/PWscf.jl
+++ src/Inputs/PWscf/PWscf.jl
@@ -30,24 +30,10 @@ export optionof, groupname
 
 include("namelists.jl")
 include("cards.jl")
-
-function iscompatible(system::SystemNamelist, cell_parameters::CellParametersCard)
-    ibrav, celldm = system.ibrav, system.celldm
-    if iszero(ibrav)
-        if optionof(cell_parameters) in ("bohr", "angstrom")
-            return all(iszero, celldm)
-        else  # "alat"
-            return !iszero(first(celldm))  # first(celldm) != 0
-        end
-    else
-        return false
-    end
-end # function iscompatible
-iscompatible(x::CellParametersCard, y::SystemNamelist) = iscompatible(y, x)
-
 include("input.jl")
 include("crystallography.jl")
 include("asstring.jl")
 include("set.jl")
+include("validation.jl")
 
 end
