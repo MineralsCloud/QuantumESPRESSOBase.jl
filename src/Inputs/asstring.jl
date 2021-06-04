023253607a03902edfdbd734f6c552e44a374655
Reno <25192197+singularitti@users.noreply.github.com>
2021-06-03 20:02:32 -0400

    Rewrite `asstring` in src/Inputs/asstring.jl using `formatconfig` methods

--- src/Inputs/asstring.jl
+++ src/Inputs/asstring.jl
@@ -1,6 +1,24 @@
-import AbInitioSoftwareBase.Inputs: asstring
+import AbInitioSoftwareBase.Inputs: FormatConfig, asstring
 
 export asstring
+
+formatconfig(::Type{QuantumESPRESSOInput}) = FormatConfig(;
+    delimiter = " ",
+    newline = "\n",
+    indent = ' '^4,
+    float = "%f",
+    int = "%i",
+    bool = ".%.",
+)
+formatconfig(::Type{<:Namelist}) = FormatConfig(;
+    delimiter = " ",
+    newline = "\n",
+    indent = ' '^4,
+    float = "%f",
+    int = "%i",
+    bool = ".%.",
+)
+formatconfig(x) = formatconfig(typeof(x))
 
 """
     asstring(input::QuantumESPRESSOInput)
@@ -8,13 +26,9 @@ export asstring
 Return a `String` representing a `QuantumESPRESSOInput`, valid for Quantum ESPRESSO's input.
 """
 function asstring(input::QuantumESPRESSOInput)
-    return join(
-        map(
-            asstring,
-            Iterators.filter(!isnothing, getfield(input, i) for i in 1:nfields(input)),
-        ),
-        newline(input),
-    ) * newline(input)  # Add a new line at the end of line to prevent errors
+    newline = formatconfig(input).newline
+    return join(map(_asstring, getfield(input, i) for i in 1:nfields(input)), newline) *
+           newline  # Add a new line at the end of line to prevent errors
 end
 """
     asstring(nml::Namelist)
@@ -22,36 +36,17 @@ end
 Return a `String` representing a `Namelist`, valid for Quantum ESPRESSO's input.
 """
 function asstring(nml::Namelist)
-    content = _nmlasstring(
-        dropdefault(nml);
-        indent = indent(nml),
-        delimiter = delimiter(nml),
-        newline = newline(nml),
-    )
-    return join(filter(!isempty, ("&" * groupname(nml), content, '/')), newline(nml))
+    config = formatconfig(nml)
+    content = _asstring(dropdefault(nml))
+    return join(filter(!isempty, ("&" * groupname(nml), content, '/')), config.newline)
 end
-asstring(x::AbstractString) = string(x)
-function _nmlasstring(dict::AbstractDict; indent = ' '^4, delimiter = ' ', newline = '\n')
-    return join(
-        map(keys(dict), values(dict)) do key, value
-            _nmlasstring(
-                key,
-                value;
-                indent = indent,
-                delimiter = delimiter,
-                newline = newline,
-            )
-        end,
-        newline,
-    )
+function _asstring(dict::AbstractDict)
+    config = formatconfig(Namelist)
+    return join((_asstring(key, value) for (key, value) in dict), config.newline)
 end
-function _nmlasstring(
-    key,
-    value::AbstractVector;
-    indent = ' '^4,
-    delimiter = ' ',
-    newline = '\n',
-)
+function _asstring(key, value::AbstractVector)
+    config = formatconfig(Namelist)
+    indent, delimiter, newline = config.indent, config.delimiter, config.newline
     return join(
         (
             indent * join((string(key, '(', i, ')'), "=", fstring(x)), delimiter) for
@@ -60,13 +55,9 @@ function _nmlasstring(
         newline,
     )
 end
-function _nmlasstring(
-    key,
-    value::NamedTuple;
-    indent = ' '^4,
-    delimiter = ' ',
-    newline = '\n',
-)
+function _asstring(key, value::NamedTuple)
+    config = formatconfig(Namelist)
+    indent, delimiter, newline = config.indent, config.delimiter, config.newline
     return join(
         map(keys(value), values(value)) do x, y
             indent * join((string(key, '%', x), "=", fstring(y)), delimiter)
@@ -74,11 +65,10 @@ function _nmlasstring(
         newline,
     )
 end
-_nmlasstring(key, value; indent = ' '^4, delimiter = ' ', newline = '\n') =
-    indent * join((string(key), "=", fstring(value)), delimiter)
-
-newline(::Union{QuantumESPRESSOInput,Namelist,Card}) = '\n'
-
-indent(::Namelist) = ' '^4
-
-delimiter(::Namelist) = ' '
+function _asstring(key, value)
+    config = formatconfig(Namelist)
+    indent, delimiter = config.indent, config.delimiter
+    return indent * join((string(key), "=", fstring(value)), delimiter)
+end
+_asstring(::Nothing) = ""
+_asstring(x) = asstring(x)
