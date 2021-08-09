8c5c3ad3d660691a4562449af52b3ef98c72a882
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-08 20:52:23 -0400

    Remove type `MonkhorstPackGrid`, import from Crystallography.jl

--- src/Inputs/PWscf/cards.jl
+++ src/Inputs/PWscf/cards.jl
@@ -1,4 +1,4 @@
-using Crystallography: Cell, ReciprocalPoint
+using Crystallography: Cell, ReciprocalPoint, MonkhorstPackGrid
 using Functors: fmap
 using StaticArrays: SVector, SMatrix
 
@@ -11,7 +11,6 @@ export AtomicSpecies,
     CellParametersCard,
     AtomicForce,
     AtomicForcesCard,
-    MonkhorstPackGrid,
     KPointsCard,
     KMeshCard,
     GammaPointCard,
@@ -209,27 +208,6 @@ function optconvert(new_option::AbstractString, card::AbstractCellParametersCard
         end)
     end
 end # function optconvert
-
-"""
-    MonkhorstPackGrid(mesh, is_shift)
-
-Represent the Monkhorst--Pack grid.
-
-# Arguments
-- `mesh`: A length-three vector specifying the k-point grid (``nk_1 × nk_2 × nk_3``) as in Monkhorst--Pack grids.
-- `is_shift`: A length-three vector specifying whether the grid is displaced by half a grid step in the corresponding directions.
-"""
-struct MonkhorstPackGrid
-    mesh::SVector{3,UInt}
-    is_shift::SVector{3,Bool}
-    function MonkhorstPackGrid(mesh, is_shift)
-        @assert all(mesh .>= 1)
-        if eltype(is_shift) != Bool
-            is_shift = Bool.(is_shift)
-        end
-        return new(mesh, is_shift)
-    end
-end
 
 abstract type KPointsCard <: Card end
 
