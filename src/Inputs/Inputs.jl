8d2c0b7e0a28724c22afde4e445ec6bd4eb40052
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-09 00:09:06 -0400

    Remove the usage of `PyFortran90Namelists`

--- src/Inputs/Inputs.jl
+++ src/Inputs/Inputs.jl
@@ -13,7 +13,6 @@ module Inputs
 
 using AbInitioSoftwareBase.Inputs: Input, InputEntry, Namelist, Card, Setter, groupname
 using Compat: only, isnothing
-using PyFortran90Namelists: fstring
 
 export optionof,
     optionpool,
