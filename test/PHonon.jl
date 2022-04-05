e2613714aa3f619d11389d79c9480163040cf730
Reno <singularitti@outlook.com>
2022-04-04 20:03:54 -0400

    Add tests "Construct a `Q2rInput`" & "Construct a `MatdynInput`: silicon"

--- test/PHonon.jl
+++ test/PHonon.jl
@@ -2,7 +2,8 @@ module PHonon
 
 using Test
 
-using QuantumESPRESSOBase.Inputs.PHonon: PhNamelist, QPointsCard, PhInput
+using QuantumESPRESSOBase.Inputs.PHonon:
+    PhNamelist, Q2rNamelist, MatdynNamelist, QPointsCard, PhInput, Q2rInput, MatdynInput
 
 @testset "Construct a `PhInput`: silicon" begin
     ph = PhNamelist(;
@@ -34,6 +35,45 @@ using QuantumESPRESSOBase.Inputs.PHonon: PhNamelist, QPointsCard, PhInput
     input = PhInput(ph, q_points)
     # Test struct equality
     @test input == PhInput(deepcopy(ph), deepcopy(q_points))
+end
+
+@testset "Construct a `Q2rInput`" begin
+    q2r = Q2rNamelist(; fildyn = "dyn", zasr = "crystal", flfrc = "fc.out")
+    input = Q2rInput(q2r)
+    # Test struct equality
+    @test input == Q2rInput(deepcopy(q2r))
+end
+
+@testset "Construct a `MatdynInput`: silicon" begin
+    matdyn = MatdynNamelist(;
+        asr = "crystal",
+        amass = [28.086],
+        flfrc = "fc.out",
+        flfrq = "freq.out",
+        flvec = "modes.out",
+        dos = true,
+        q_in_band_form = false,
+        nk1 = 8,
+        nk2 = 8,
+        nk3 = 8,
+    )
+    q_points = QPointsCard(
+        [
+            0.125 0.125 0.125 1.0
+            0.125 0.125 0.375 3.0
+            0.125 0.125 0.625 3.0
+            0.125 0.125 0.875 3.0
+            0.125 0.375 0.375 3.0
+            0.125 0.375 0.625 6.0
+            0.125 0.375 0.875 6.0
+            0.125 0.625 0.625 3.0
+            0.375 0.375 0.375 1.0
+            0.375 0.375 0.625 3.0
+        ],
+    )
+    input = MatdynInput(matdyn, q_points)
+    # Test struct equality
+    @test input == MatdynInput(deepcopy(matdyn), deepcopy(q_points))
 end
 
 end
