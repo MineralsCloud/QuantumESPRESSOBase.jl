bfd4478c6df1f27f4748fa7db0ff87bd89d80658
singularitti <singularitti@outlook.com>
2023-10-09 16:06:48 -0400

    Move `find_symmetry` from src/PWscf/crystallography.jl to ext/CrystallographyExt.jl

---
+++ ext/CrystallographyExt.jl
@@ -0,0 +1,29 @@
+module CrystallographyExt
+
+using Crystallography: Lattice, Cell, getsymmetry
+using Unitful: ustrip, @u_str
+
+function find_symmetry(input::PWInput, symprec=1e-5)
+    lattice = Lattice(input)
+    option = input.atomic_positions.option
+    data = Iterators.map(input.atomic_positions.data) do atomic_position
+        atom, position = atomic_position.atom, atomic_position.pos
+        # `position` is a `Vector` in unit of "bohr"
+        if option == "alat"
+            position *= input.system.celldm[1]
+        elseif option == "bohr"
+            position
+        elseif option == "angstrom"
+            ustrip.(u"bohr", position * u"angstrom")
+        elseif option == "crystal"
+            lattice(position)
+        else  # option == "crystal_sg"
+            error("unimplemented!")  # FIXME
+        end
+        position, atom
+    end
+    cell = Cell(lattice, first.(data), last.(data))
+    return getsymmetry(cell, symprec)
+end
+
+end
