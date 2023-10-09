56236572afbf172dd59186fe21e7c3ae2469502d
singularitti <singularitti@outlook.com>
2023-10-09 16:08:28 -0400

    Fix import

--- ext/CrystallographyExt.jl
+++ ext/CrystallographyExt.jl
@@ -4,6 +4,8 @@ using Crystallography: Lattice, Cell
 using QuantumESPRESSOBase.PWscf: PWInput
 using Unitful: ustrip, @u_str
 
+import CrystallographyBase: getsymmetry
+
 function getsymmetry(input::PWInput, symprec=1e-5)
     lattice = Lattice(input)
     option = input.atomic_positions.option
